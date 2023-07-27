f4f452c836ba906ab00e009346a5db35cd4da51b
sara <sara.atef001@gmail.com>
2023-07-27 11:57:32 +0300

    Day01

---
+++ Day01/JS/demo.js
@@ -0,0 +1,208 @@
+/**
+ * Agenda
+ * -------------------
+ *  1- What is javascript , Javascript (History)
+ *  2- where is js code  written ?
+ *  3- datatypes
+ *  4- falsy Values
+ *  4- Operators
+ *  5- (if,switch,loop,for)
+ *  6-Communcation with user
+ * (regx) ==> self study
+ */
+/***
+ * Datatype
+ * -----------------------
+ * == Primitve Datatype
+ * -----------------------
+ * -- string
+ * -- number
+ * -- boolean
+ * -- undefined
+ * -- null
+ * --------------------------
+ * == Reference Datatype
+ * -- Object
+ * ---------------------------
+ * == NaN --> Not A Number
+ * 
+/**
+ * Comparasion Operators
+ * --------------------------
+ *  [==]  ---> (values Only)
+ *  [===] ---> (values & datatype)
+ * /**
+ * 1- Arithmatic Operator (+,-,*,/)
+ * -----------------------------------
+ * (+) ==> Number + number ==> Add
+ * (+) ==> string + string ==> concat
+ * Coercion ==> convert automatic from datatype to anther datatype
+ * 
+ * /** falsy values
+ *  == false
+ *  == 0
+ *  == undefined
+ *  == NaN
+ *  == ""
+    == null
+*/
+
+// var fname = "mona"; // dreclation
+
+// fname = "ali"; // reassign
+// fname = 10;
+// fname = true;
+// console.log(typeof fname);
+// var num = 20.23668;
+// var flag = true;
+// var student;
+
+// student = "ali";
+// console.log(typeof student);
+
+// var emp = "Ali";
+// emp = "alaa";
+
+// emp = 10;
+// emp = true;
+// console.log(emp);
+
+// var fname = 10;
+// var fname = String(10);
+// console.log(typeof fname);
+
+// var student = "Ali";
+// var student2 = 10;
+
+// console.log(student);
+
+// var num = 10;
+// var num = Number("mona");
+// console.log(num);
+// console.log(typeof num);
+
+// var flag = true;
+// var flag = Boolean(true);
+
+// console.log(flag);
+
+// var product = null;
+
+// console.log(typeof product);
+// var flag = Boolean(345678);
+
+// console.log(flag);
+
+// var num2 = 10; // number
+// var num1 = "mona"; // string
+// console.log(num2 + num1);
+
+// var num1 = "true"; //NaN  -->   number
+// var num2 = true; // 1    -->  number
+
+// console.log(num1 === num2);
+// var num1 = undefined; // false
+
+// var num2 = null; // false
+
+// console.log(num1 === num2);
+/**-------------
+ * Logical Operator
+ *
+ * = and ==> &&
+ *
+ * true && false ==> false
+ * false && true ==> false
+ * false && false ==> false
+ * true && true ==> true
+ * --------------------------------
+ *  OR ==> ||
+ * --------------------------------
+ * true || true --> true
+ * true || false --> true
+ * false || true --> true
+ * false || false --> false
+ *------------------------------
+ Not 
+    false ==> true
+    true ==> false
+ * ------------- */
+
+// "mona" && "ola" ==> "ola";
+// true && true  --> true
+
+// 0 && 'ali' ==> 0
+
+// false &&
+// a = a || 0;
+// a= 0 ,, mona
+
+/**================= */
+
+// for (var i = 0; i < 5; i++) {
+// 	console.log(i);
+// }
+
+// var i = 0;
+// while (i < 5) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// var i = 0;
+// do {
+// 	console.log(i);
+// 	i++;
+// } while (i < 5);
+
+/** Terary Operator */
+// var color = "green";
+
+// // var flag = true;
+// // if (flag) console.log("Ok");
+// // else console.log(":(");
+
+// color == "red" ? console.log("Ok") : console.log(":(");
+// var fname = prompt("Enter Your Name");
+
+// var num1 = prompt("Enter Num1");
+// var num2 = prompt("Enter Num2");
+// console.log(Number(num1) + Number(num2));
+
+var i = 1;
+document.writeln("<h" + i + " class='test'>Hello JS</h" + i + ">");
+
+// var result = confirm("Are you Sure You want to delete this element");
+
+// console.log(result);
+// result
+// 	? document.writeln("Item Delete")
+// 	: document.writeln("Operation Canceld");
+// alert("operation Canceled");
+// /**
+//  * 10
+//  * 20
+//  * 30
+//  * 50
+//  *
+//  * 110
+//  *
+//  *
+//  * 20
+//  * 10
+//  * 0
+//  * 30
+//  *
+//  */
+
+/***
+ *
+ * Ahmed ---> 1
+ * mona --> 0
+ * Hellooo ee --> 3
+ */
+/**
+ * ITI --> iTI -->
+ * MOOM
+ *
+ */
