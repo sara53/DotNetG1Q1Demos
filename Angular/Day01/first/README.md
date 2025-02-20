0d163ee48626eb3036293f38ae8e272427b7b81d
sara <sara.atef001@gmail.com>
2025-02-20 11:58:14 +0200

    day01

---
+++ Angular/Day01/first/README.md
@@ -0,0 +1,8 @@
+# ng new projectName
+
+# ng serve -o
+
+# to create angular component
+
+- ng g c componentName
+  -- ng g c componentName --skip-tests --flat
---
+++ Angular/Day01/first/src/app/components/content/content.compoennt.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css',
+})
+export class ContentComponent {}
